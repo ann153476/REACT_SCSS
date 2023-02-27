019bf78ca752fc5088d4ea051decfd04f1c3d19e
Anka <103377387+ann153476@users.noreply.github.com>
2023-02-27 15:10:39 +0200

    1

---
+++ src/components/Test/Test.jsx
@@ -0,0 +1,11 @@
+import s from './test.module.scss';
+const Test = () => {
+  return (
+    <>
+      <h1 className={s.color}>hello</h1>
+      <p>iygiygyhg</p>
+    </>
+  );
+};
+
+export default Test;
